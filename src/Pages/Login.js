b002dcd25e68ac90019c4a47d41024177efbd84d
Alec <alecmichaelmiller@gmail.com>
2020-12-09 20:40:54 -0700

    some CSS

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,26 +1,29 @@
 import { Component, React } from 'react';
 
-class LoginPage extends Component{
-    constructor(props) {
-      super(props);
-      this.checkLogin = this.checkLogin.bind(this)
-    }
-  
-    checkLogin(){
-        var api_key = document.getElementById('api_key').value;
-        this.props.checkLogin(api_key);
-    }
-  
-    render(){
-      return(
-        <div>
-            <h1>ようこそ!</h1>
-            <p>Please enter your API key</p>
-            <input type="text" id="api_key"/><br/>
-            <button onClick={this.checkLogin}>Login</button>
-        </div>
-      )
-    }
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.checkLogin = this.checkLogin.bind(this)
   }
 
-  export default LoginPage
+  checkLogin() {
+    var api_key = document.getElementById('api_key').value;
+    this.props.checkLogin(api_key);
+  }
+
+  render() {
+    return (
+      <div className='login'>
+        <div className='login_content'>
+          <h1 className='title'>ようこそ!</h1>
+          <p className='login_description'>Please enter your API key</p>
+          <input type="text" id="api_key" /><br />
+          <button onClick={this.checkLogin}>Login</button>
+          <p>Get your API key <a href="https://www.wanikani.com/settings/personal_access_tokens" target="_blank">here</a></p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LoginPage
